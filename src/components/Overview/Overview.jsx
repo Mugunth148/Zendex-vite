6c3f4c75ca2f7c6b948fe3a1acc9d075b03f2dd5
Mugunth148 <Mugunth148@gmail.com>
2023-08-20 20:48:42 +0530

    added overview cards

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -6,16 +6,32 @@ function Overview() {
   return (
     <>
         <div className="overview">
-            <span>Overview</span>
-            <div className="overview_header">
+
+            <div className="overview-header">
+                <span>Overview</span>
                 <h1>Overview</h1>
             </div>
-            <div className="overview-content">
-             <div className="overview-l"></div>
-             <div className="overview-r"></div>
 
+            <div className="overview-content">
+                  <div className="overview-l-main">
+
+                  <div className="overview-l item-1">
+                    <div className="item" id='item-1'></div>
+                  </div>
+
+                  <div className="overview-l item-2">
+                  <div className="item" id='item-2'></div>
+                  </div>
+
+                  <div className="overview-l item-3">
+                  <div className="item" id='item-3'></div>
+                  </div>
+
+                  </div>
+             <div className="overview-r"></div>
             </div>
         </div>
+        <div className="spacer"></div>
     </>
   )
 }
